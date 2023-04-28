3153fa59344eca81f4dafd07645daf12f5327f96
ollily <coding@glowa-net.com>
2023-04-28 20:16:26 +0200

    [global-pom] #1 : New CI job - update gitflow actions

--- .mvn/.build.sh
+++ .mvn/.build.sh
@@ -44,7 +44,7 @@ show_build() {
 
 show_repo() {
   echo -e "\n**** show_repo ****\n"
-  du --max-depth=1 -h "${MVN_REPO_JOB_DIR}"
+  du --max-depth=2 -h "${MVN_REPO_JOB_DIR}"
 }
 
 prepare_upload() {
@@ -104,7 +104,7 @@ source "${SCRIPT_FOLDER}/.env.sh"
 if [ 0 -eq ${DEBUG} ]; then
     show_env
     show_mvnsettings
-    show_pom
+    #show_pom
 fi
 
 # 3. build & show result
